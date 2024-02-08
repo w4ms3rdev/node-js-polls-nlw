c24ca1227174108f2307f7e56dd9e31f44c1922e
Artur Wamser <116298817+w4ms3rdev@users.noreply.github.com>
2024-02-08 11:49:15 -0300

    node-js-projct

---
+++ prisma/migrations/20240206232632_fix_votes/migration.sql
@@ -0,0 +1,34 @@
+/*
+  Warnings:
+
+  - You are about to drop the `vote` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "vote" DROP CONSTRAINT "vote_pollId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "vote" DROP CONSTRAINT "vote_pollOptionId_fkey";
+
+-- DropTable
+DROP TABLE "vote";
+
+-- CreateTable
+CREATE TABLE "Vote" (
+    "id" SERIAL NOT NULL,
+    "sessionId" TEXT NOT NULL,
+    "pollOptionId" TEXT NOT NULL,
+    "pollId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Vote_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Vote_sessionId_pollId_key" ON "Vote"("sessionId", "pollId");
+
+-- AddForeignKey
+ALTER TABLE "Vote" ADD CONSTRAINT "Vote_pollOptionId_fkey" FOREIGN KEY ("pollOptionId") REFERENCES "PollOption"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Vote" ADD CONSTRAINT "Vote_pollId_fkey" FOREIGN KEY ("pollId") REFERENCES "Poll"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
